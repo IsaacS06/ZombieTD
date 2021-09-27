fc443779640f43cfe20ea81ce1064f7e777e52be
Isaac Street <isaacstreet@Isaacs-Macbook-Air.local>
2021-09-27 18:58:01 -0400

    Added Zombies To The Game, and resources + tables

--- core/src/com/main/Main.java
+++ core/src/com/main/Main.java
@@ -4,28 +4,43 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import java.util.ArrayList;
+
 
 public class Main extends ApplicationAdapter {
+	//TODO: GAME VARIABLES
 	SpriteBatch batch;
-	Texture img;
+
+	//TODO: GAME LISTS\
+	ArrayList<Zombie> zombies = new ArrayList<Zombie>();
+
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		for (int i = 0; i < 1000; i ++){
+			zombies.add(new Zombie("zzz", 526 + i * 50, 300, 5));
+		}
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0, 0, 0, 1);
+		update();
 		batch.begin();
-		batch.draw(img, 0, 0);
+		for(Zombie z : zombies) z.draw(batch);
 		batch.end();
 	}
-	
+
+	public void update(){
+		for(Zombie z : zombies) z.update();
+	}
+
+
+	//*******************END OF FILE*******************\\
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
